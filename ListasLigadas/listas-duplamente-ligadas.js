2072c3354d40195c027b3e8e33e989c776beadc9
Leonardo <leoprofissional@live.com>
2021-01-05 06:37:00 -0300

    update 06/01/2021

--- ListasLigadas/listas-duplamente-ligadas.js
+++ ListasLigadas/listas-duplamente-ligadas.js
@@ -71,6 +71,36 @@ function DoublyLinkedList(){
 
     this.removeAt = function(position){
         //remove o elemento de uma posição específica
+        if(position > -1 && position < length){
+            var current = head,
+            previous,
+            index = 0
+
+            if(position === 0){
+                head = current.next
+
+                if(length === 1){
+                    tail = null
+                } else{
+                    head.prev = null
+                }
+            } else if(position === length -1){
+                current = tail
+                tail = current.prev
+                tail.next = null
+            } else{
+                while(index++ < position){
+                    previous = current
+                    current = current.next
+                }
+                previous.next = current.next
+                current.next.prev = previous
+            }
+            length--
+            return current.element
+        } else {
+            return null
+        }
         
     }
 
@@ -136,4 +166,10 @@ dll.append('Maria')
 dll.insert(0, 'Carlos')
 dll.insert(4, 'Lucas')
 dll.insert(2, 'Ana')
+dll.print()
+dll.removeAt(0)
+dll.print()
+dll.removeAt(2)
+dll.print()
+dll.removeAt(3)
 dll.print()
