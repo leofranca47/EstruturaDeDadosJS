7da5f95521146c8979b9062aaa31f82553fd55a4
Leonardo <leoprofissional@live.com>
2021-01-25 06:28:29 -0300

    update 25/01/2021

--- Arvores/arvores.js
+++ Arvores/arvores.js
@@ -53,14 +53,72 @@ function BinarySearchTree(){
 
     this.remove = function(key){
         // remove uma chave
+        root = removeNode(root, key)
+    }
+
+    var removeNode = function(node, key){
+        if(node === null){
+            return null
+        }
+        if(key < node.key){
+            node.left = removeNode(node.left, key)
+            return node
+        } else if(key > node.key){
+            node.right = removeNode(node.right, key)
+            return node
+        } else {
+            if(node.left === null && node.right === null){
+                node = null
+                return node
+            }
+            if(node.left === null){
+                node = node.right
+                return node
+            } else if(node.right === null){
+                node = node.left
+                return node
+            }
+            var aux = findMinNode(node.right)
+            node.key = aux.key
+            node.right = removeNode(node.right, aux.key)
+            return node
+        }
     }
 
     this.min = function(){
         // retorna a menor chave
+        return minNode(root)
+    }
+
+    var minNode = function(node){
+        if(node){
+            while(node && node.left !== null){
+                node = node.left
+            }
+            return null
+        }
+    }
+
+    var findMinNode = function(node){
+        while(node && node.left !== null){
+            node = node.left
+        }
+        return node
     }
 
     this.max = function(){
         // retorna a maior chave
+        return maxNode(root)
+    }
+
+    var maxNode = function(node){
+        if(node){
+            while(node && node.right !== null){
+                node = node.right
+            }
+            return node.key
+        }
+        return null
     }
 
     this.inOrderTraverse = function(){
